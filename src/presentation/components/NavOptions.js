7f54d1a37a4c39d48d20e566753c6887fb9052c6
Lawrence Macharia <macharialawrence2011@gmail.com>
2021-08-09 08:16:53 +0300

    initial commit, navigation options that appear on the home screen

---
+++ src/presentation/components/NavOptions.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen"
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatScreen"
+  }
+]
+
+const NavOptions = () => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image style={{ width: 120, height: 120, resizeMode: 'contain' }} source={{ uri: item.image }} />
+            <Text>{item.title}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  )
+}
+
+export default NavOptions
