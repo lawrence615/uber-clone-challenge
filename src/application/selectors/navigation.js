774db1a6ca4c4ac17a93b7f7b62d276087c1d171
Lawrence Macharia <macharialawrence2011@gmail.com>
2021-08-10 12:21:41 +0300

    renamed to navSlice

--- src/application/selectors/navigation.js
+++
@@ -1,3 +0,0 @@
-export const selectOrigin = state => state.nav.origin
-export const selectDestination = state => state.nav.destination
-export const selectTravelTimeInformation = state => state.nav.travelTimeInformation
