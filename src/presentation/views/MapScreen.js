8dcd99f1313aa77cfb98efb91fa2dae2e91ce187
Lawrence Macharia <macharialawrence2011@gmail.com>
2021-08-15 20:53:45 +0300

    Added home navigation button

--- src/presentation/views/MapScreen.js
+++ src/presentation/views/MapScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native';
 
 import Map from 'components/Map'
 import NavigateCard from 'components/NavigateCard';
@@ -10,9 +12,15 @@ import RideOptionsCard from 'components/RideOptionsCard';
 const MapScreen = () => {
 
   const Stack = createNativeStackNavigator()
+  const navigation = useNavigation()
 
   return (
     <View>
+      <TouchableOpacity
+        style={tw` bg-gray-100 absolute top-10 left-8 z-50 p-3 rounded-full shadow-lg`}
+        onPress={() => navigation.navigate('HomeScreen')}>
+        <Icon name="menu" />
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
